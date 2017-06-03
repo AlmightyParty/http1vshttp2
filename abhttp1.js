6a25b78ebb23a072ac30c641d957fa2df2078045
yangchongduo <yangchongduo@qudian.com>
2017-06-03 17:55:17 +0800

    first commit

---
+++ abhttp1.js
@@ -0,0 +1,36 @@
+// Server Software:
+// 	Server Hostname:        47.94.95.52
+// Server Port:            8081
+// SSL/TLS Protocol:       TLSv1.2,ECDHE-RSA-AES128-GCM-SHA256,4096,128
+//
+// Document Path:          /item1.html
+// Document Length:        48738 bytes
+//
+// Concurrency Level:      100
+// Time taken for tests:   178.441 seconds
+// Complete requests:      385
+// Failed requests:        0
+// Total transferred:      20482534 bytes
+// HTML transferred:       20369762 bytes
+// Requests per second:    2.16 [#/sec] (mean)
+// Time per request:       46348.339 [ms] (mean)
+// Time per request:       463.483 [ms] (mean, across all concurrent requests)
+// Transfer rate:          112.10 [Kbytes/sec] received
+//
+// Connection Times (ms)
+// min  mean[+/-sd] median   max
+// Connect:       13 2934 6886.7    645   52871
+// Processing:  1967 26743 20364.8  19744  101321
+// Waiting:        2   97 1325.6      4   25988
+// Total:       2201 29677 21851.3  22634  124310
+//
+// Percentage of the requests served within a certain time (ms)
+// 50%  22624
+// 66%  32357
+// 75%  39824
+// 80%  44444
+// 90%  65867
+// 95%  75904
+// 98%  85192
+// 99%  96120
+// 100%  124310 (longest request)
